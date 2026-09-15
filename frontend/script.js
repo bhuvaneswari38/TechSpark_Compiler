05ea9ea74f49d426f468487f208a64d8af58b5f0
bhuvaneswari38 <bhuvaneswari3121@gmail.com>
2026-09-15 22:41:07 +0530

    Fix Docker configuration and frontend backend connection

--- frontend/script.js
+++ frontend/script.js
@@ -190,7 +190,7 @@ async function runCode(){
 
     try{
 
-        const response = await fetch("https://contained-likes-grand-led.trycloudflare.com/run",{
+        const response = await fetch("http://localhost:5000/deploy",{
 
             method:"POST",
 
